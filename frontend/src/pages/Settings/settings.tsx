08024bf620434730827e5f95faa8a297a33248f8
Daniel <100786770+dr0pshot03@users.noreply.github.com>
2026-02-16 11:14:33 +0100

    minor changes

--- frontend/src/pages/Settings/settings.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Container, Flex, HStack, Spacer, Icon, Image } from "@chakra-ui/react";
-import { FiSettings } from "react-icons/fi";
-import { InlineIcon } from "@iconify/react";
-
-export default function Settings () {
-    return(
-        
-    )
-}
